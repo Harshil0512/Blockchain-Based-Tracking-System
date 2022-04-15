e223b1f3290de2037d7bf5bdb589b324ed216ca3
Harshil Khamar <harshilkhamar1@gmail.com>
2022-04-15 22:45:07 +0530

    Db Integration Done

--- dbconnect.php
+++
@@ -1,6 +0,0 @@
-<?php
-$con=mysqli_connect("localhost","root","","blockchain");
-if(!$con){
-echo "Database connection not success!!";
-}
-?>
---
+++ dbconnection.php
@@ -0,0 +1,7 @@
+<?php
+    $con = mysqli_connect("localhost","root","","BlockChain");
+    if(!$con)
+    {
+        exit("Failed To Connect");
+    }
+?>
