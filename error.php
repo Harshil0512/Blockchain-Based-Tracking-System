7b76cdbce0d0672c6dd106bfb3a35975285f10a7
hemansi-123 <hemansichalodiya16@gmail.com>
2022-04-17 14:09:55 +0530

    success error messeges done

---
+++ error.php
@@ -0,0 +1,77 @@
+<?php
+require 'dbconnection.php';
+
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="//cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
+
+<style>
+    h2 {
+  position: relative;
+  padding: 0;
+  margin: 0;
+  font-family: "Raleway", sans-serif;
+  font-weight: 300;
+  font-size: 40px;
+  color: #080808;
+  -webkit-transition: all 0.4s ease 0s;
+  -o-transition: all 0.4s ease 0s;
+  transition: all 0.4s ease 0s;
+  margin:20px;
+}
+    .seven h2 {
+text-align: center;
+    font-size:30px; font-weight:300; color:#222; letter-spacing:1px;
+    text-transform: uppercase;
+
+    display: grid;
+    grid-template-columns: 1fr max-content 1fr;
+    grid-template-rows: 27px 0;
+    grid-gap: 20px;
+    align-items: center;
+}
+
+.seven h2:after,.seven h2:before {
+    content: " ";   
+    display: block;
+    border-bottom: 1px solid #c50000;
+    border-top: 1px solid #c50000;
+    height: 5px;
+    background-color:#f8f8f8;
+}
+</style>
+    <title>Error</title>
+
+</head>
+
+<body>
+    <!-- <div class="seven">
+    <h2>History</h2>
+    </div> -->
+    <div class="container my-4">
+    <div class="seven">
+    <h2>Error</h2>
+    </div>
+        <h3> Your Transaction is Failed!!</h3><br>
+        <h3>Try Again.</h3>
+    </div>
+    
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="//cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+   
+
+</body>
+
+</html>
---
+++ success.php
@@ -0,0 +1,128 @@
+<?php
+session_start();
+require 'dbconnection.php';
+
+$id= $_SESSION['AccountId'];
+
+?>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="//cdn.datatables.net/1.10.20/css/jquery.dataTables.min.css">
+
+<style>
+    h2 {
+  position: relative;
+  padding: 0;
+  margin: 0;
+  font-family: "Raleway", sans-serif;
+  font-weight: 300;
+  font-size: 40px;
+  color: #080808;
+  -webkit-transition: all 0.4s ease 0s;
+  -o-transition: all 0.4s ease 0s;
+  transition: all 0.4s ease 0s;
+  margin:20px;
+}
+    .seven h2 {
+text-align: center;
+    font-size:30px; font-weight:300; color:#222; letter-spacing:1px;
+    text-transform: uppercase;
+
+    display: grid;
+    grid-template-columns: 1fr max-content 1fr;
+    grid-template-rows: 27px 0;
+    grid-gap: 20px;
+    align-items: center;
+}
+
+.seven h2:after,.seven h2:before {
+    content: " ";   
+    display: block;
+    border-bottom: 1px solid #c50000;
+    border-top: 1px solid #c50000;
+    height: 5px;
+  background-color:#f8f8f8;
+}
+</style>
+    <title>Success</title>
+
+</head>
+
+<body>
+    <!-- <div class="seven">
+    <h2>History</h2>
+    </div> -->
+    <div class="container my-4">
+    <div class="seven">
+    <h2>Success</h2>
+    </div>
+        <table class="table" id="myTable">
+            <thead>
+                <tr>
+                    <th scope="col">Date And Time</th>
+                    <th scope="col">Amount Transfer</th>
+					<th scope="col">From</th>
+                    <th scope="col">To</th>
+                    <th scope="col">From Type</th>
+                    <th scope="col">To Type</th>
+                   
+                </tr>
+            </thead>
+            <tbody>
+
+                <!-- <tr>
+                    <th scope='row'>1</th>
+                    <td>18956</td>
+                    <td>187452</td>
+                    <td>50000</td>
+                    <td>Govt to pvt</td>
+                    <td>24/08/2002</td>
+                </tr> -->
+
+                <?php 
+          $sql = "select * from trasactionhistory where FromAccId ='$id'";
+          $result = mysqli_query($con, $sql);
+          $row=$result->mysqli_fetch_assoc();
+
+         
+            echo "<tr>
+            
+            <td>". $row['date'] . "</td>
+            <td>". $row['amt'] . "</td>
+            <td>". $row['Amount'] . "</td>
+            <td>". $row2['Mode'] . "</td>
+            <td>". $row['Date'] . "</td>
+            <td>". $row['Date'] . "</td>
+
+          </tr>";
+       ?>
+
+            </tbody>
+        </table>
+    </div>
+    <hr>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+    <script src="//cdn.datatables.net/1.10.20/js/jquery.dataTables.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            $('#myTable').DataTable();
+
+        });
+    </script>
+
+</body>
+
+</html>
